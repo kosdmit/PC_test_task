df18f92c078c672f7a8fcb3cb5edad9f96ad2781
kosdmit <kosdmit@hotmail.com>
2023-10-08 21:05:58 +0400

    Updated save method of DataFile method for getting file name and columns

--- app_api/models.py
+++ app_api/models.py
@@ -1,3 +1,7 @@
+import csv
+import io
+import json
+
 from django.db import models
 
 
@@ -7,3 +11,16 @@ class DataFile(models.Model):
     file = models.FileField(upload_to="files/")
     uploaded_at = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=True)
     columns = models.TextField(editable=False, null=False, blank=True)  # JSON serialized list of columns.
+
+    def save(self, *args, **kwargs):
+        if self.file:
+            self.name = self.file.name
+
+            file = self.file.read().decode('utf-8')
+            csv_data = csv.reader(io.StringIO(file), delimiter=',')
+
+            headers = next(csv_data)  # Get the first row, which should be the headers/column names
+            self.columns = json.dumps(headers)
+
+        super().save(*args, **kwargs)
+
